354a87adfbc048ad5fdc0b1995679c61e09c1630
Quinn Marshall <qballboy@gmail.com>
2015-05-27 09:56:47 -0700

    changed formatting

--- Snake/src/Game.java
+++ Snake/src/Game.java
@@ -14,76 +14,69 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Game extends JFrame
-{
-  private BufferedImage backgroundPic;
-  private Color         snakeColor;
-  private int           speed;
-  private Color         backgroundColor;
-  public Game()
-  {
-    super("Snake");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setResizable(false);
-    setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
-    setUndecorated(true);
-    setLocationRelativeTo(null);
-    BufferedImage myPicture;
-    try
-    {
-      myPicture = ImageIO.read(new File("H:\\Java\\git\\snake\\Snake\\src\\Blue Snake.jpg"));
-      final JLabel background = new JLabel(new ImageIcon(myPicture));
-      add(background);
-      background.setLayout(new FlowLayout());
-      final JButton option = new JButton();
-      option.setText("Options");
-      option.setBackground(Color.WHITE);
-      background.add(option);
-      final JButton quit = new JButton();
-      quit.setText("Quit");
-      quit.addActionListener(new ActionListener()
-      {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-          System.exit(0);
-        }
-      });
-      background.add(quit);
-      final JButton play = new JButton();
-      play.setText("Play");
-      play.setBackground(Color.WHITE);
-      background.add(play);
-      play.addActionListener(new ActionListener()
-      {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-          //start game
-          GameBoard board;
-          speed = 1;
-          snakeColor = Color.RED;
-          backgroundColor = Color.MAGENTA;
-          board = new GameBoard(speed, snakeColor, backgroundColor);
-          background.add(board);
-          background.remove(play);
-          background.remove(quit);
-          background.remove(option);
-          repaint();
-          while (board.play())
-          {
-          }
-          remove(board);
-          background.add(play);
-          background.add(option);
-          background.add(quit);
-        }
-      });
-    }
-    catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-    setVisible(true);
-  }
+public class Game extends JFrame {
+	
+	private BufferedImage backgroundPic;
+	private Color snakeColor;
+	private int speed;
+	private Color backgroundColor;
+
+	public Game() {
+		super("Snake");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+		setUndecorated(true);
+		setLocationRelativeTo(null);
+		BufferedImage myPicture;
+		try {
+			myPicture = ImageIO.read(new File(
+					"H:\\Java\\git\\snake\\Snake\\src\\Blue Snake.jpg"));
+			final JLabel background = new JLabel(new ImageIcon(myPicture));
+			add(background);
+			background.setLayout(new FlowLayout());
+			final JButton option = new JButton();
+			option.setText("Options");
+			option.setBackground(Color.WHITE);
+			background.add(option);
+			final JButton quit = new JButton();
+			quit.setText("Quit");
+			quit.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.exit(0);
+				}
+			});
+			background.add(quit);
+			final JButton play = new JButton();
+			play.setText("Play");
+			play.setBackground(Color.WHITE);
+			background.add(play);
+			play.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// start game
+					GameBoard board;
+					speed = 1;
+					snakeColor = Color.RED;
+					backgroundColor = Color.MAGENTA;
+					board = new GameBoard(speed, snakeColor, backgroundColor);
+					background.add(board);
+					background.remove(play);
+					background.remove(quit);
+					background.remove(option);
+					repaint();
+					while (board.play()) {
+					}
+					remove(board);
+					background.add(play);
+					background.add(option);
+					background.add(quit);
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		setVisible(true);
+	}
 }
--- Snake/src/GameBoard.java
+++ Snake/src/GameBoard.java
@@ -4,36 +4,36 @@ import java.awt.Toolkit;
 
 import javax.swing.JLabel;
 
-public class GameBoard extends JLabel
-{
-  private Snake snake;
-  private int   score;
-  private Color backgroundColor;
-  private Fruit fruit;
-  public GameBoard(int diff, Color snakeColor, Color backgroundColor)
-  {
-    super();
-    setBackground(backgroundColor);
-    setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
-    score = 0;
-    snake = new Snake(diff, snakeColor);
-    fruit = new Fruit();
-    add(fruit);
-    setOpaque(true);
-    setVisible(true);
-  }
-  public boolean play()
-  {
-    snake.move();
-    if (snake.isOn(fruit))
-    {
-      score += 1;
-    }
-    while (snake.isOn(fruit))
-    {
-      fruit.move();
-    }
-    if (snake.isDead()) { return false; }
-    return true;
-  }
+public class GameBoard extends JLabel {
+	
+	private Snake snake;
+	private int score;
+	private Color backgroundColor;
+	private Fruit fruit;
+
+	public GameBoard(int diff, Color snakeColor, Color backgroundColor) {
+		super();
+		setBackground(backgroundColor);
+		setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+		score = 0;
+		snake = new Snake(diff, snakeColor);
+		fruit = new Fruit();
+		add(fruit);
+		setOpaque(true);
+		setVisible(true);
+	}
+
+	public boolean play() {
+		snake.move();
+		if (snake.isOn(fruit)) {
+			score += 1;
+		}
+		while (snake.isOn(fruit)) {
+			fruit.move(snake);
+		}
+		if (snake.isDead()) {
+			return false;
+		}
+		return true;
+	}
 }
