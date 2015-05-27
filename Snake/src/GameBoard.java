af33e23772673b8baf26e193c3e6f3b54aa78f73
quinn marshall <qballboy@Gmail.com>
2015-05-27 10:14:35 -0700

    Corrected compile errors

--- Snake/src/GameBoard.java
+++ Snake/src/GameBoard.java
@@ -1,43 +1,6 @@
-<<<<<<< HEAD
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
-import javax.swing.JLabel;
-
-public class GameBoard extends JLabel {
-	private Snake snake;
-	private int score;
-	private Color backgroundColor;
-	private Fruit fruit;
-
-	public GameBoard(int diff, Color snakeColor, Color backgroundColor) {
-		super();
-		setBackground(backgroundColor);
-		setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
-		score = 0;
-		snake = new Snake(diff, snakeColor);
-		fruit = new Fruit();
-		add(fruit);
-		setOpaque(true);
-		setVisible(true);
-	}
-
-	public boolean play() {
-		snake.move();
-		if (snake.isOn(fruit)) {
-			score += 1;
-			fruit.move(snake);
-		}
-		repaint();
-		return snake.isAlive();
-	}
-}
-=======
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 import javax.swing.JLabel;
 
 public class GameBoard extends JLabel {
