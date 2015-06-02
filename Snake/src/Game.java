dbf2b1203acb09a5b07a8aeaf1a25306f911d92e
Quinn Marshall <qballboy@gmail.com>
2015-06-02 13:43:38 -0700

    Game changes

--- Snake/src/Game.java
+++ Snake/src/Game.java
@@ -17,94 +17,27 @@ public class Game extends JFrame {
 
 	private JFrame j = new JFrame();
 	private BufferedImage backgroundPic;
+	private GameBoard board;
+	private Timer timer;
+
 	private Color snakeColor;
 	private int speed;
 	private Color backgroundColor;
 	private boolean running = false;
-	private GameBoard board;
-	private Timer timer;
-	private ActionListener actListener;
+
 	public static final int WIDTH = 1200;
 	public static final int HEIGHT = 700;
 
 	public Game() {
 
-		super("Snake");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setSize(WIDTH, HEIGHT);
-		setUndecorated(false);
+		pack();
+
+		setTitle("Snake");
 		setLocationRelativeTo(null);
-		BufferedImage myPicture;
-
-		try {
-
-			myPicture = ImageIO.read(new File("Blue Snake.jpg"));
-			final JLabel background = new JLabel(new ImageIcon(myPicture));
-			add(background);
-			background.setLayout(new FlowLayout());
-
-			final JButton option = new JButton();
-			option.setText("Options");
-			option.setBackground(Color.WHITE);
-
-			background.add(option);
-
-			final JButton quit = new JButton();
-			quit.setText("Quit");
-			quit.addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent e) {
-					System.exit(0);
-				}
-			});
-			background.add(quit);
-
-			final JButton play = new JButton();
-			play.setText("Play");
-			play.setBackground(Color.WHITE);
-			background.add(play);
-			play.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-
-					// start game
-					speed = 2;
-					snakeColor = Color.RED;
-					backgroundColor = Color.BLACK;
-					board = new GameBoard(snakeColor, backgroundColor);
-
-					add(board);
-					board.repaint();
-					setVisible(true);
-					background.setVisible(false);
-
-					actListener = new ActionListener() {
-						public void actionPerformed(ActionEvent event) {
-							if (!isRunning()) {
-								timer.stop();
-							}
-							run();
-							board.repaint();
-						}
-					};
-					setVisible(true);
-
-					timer = new Timer(1000 / (speed * 10), actListener);
-					timer.start();
-
-					if (!timer.isRunning()) {
-						remove(board);
-						background.setVisible(true);
-						setVisible(true);
-					}
-
-					setVisible(true);
-				}
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		menu();
 	}
 
 	public void run() {
@@ -126,6 +59,76 @@ public class Game extends JFrame {
 
 	public static void pause() {
 
+	}
+
+	public void menu() {
+		try {
+
+			BufferedImage myPicture = ImageIO.read(new File("Blue Snake.jpg"));
+			final JLabel background = new JLabel(new ImageIcon(myPicture));
+			add(background);
+			background.setLayout(new FlowLayout());
+
+			final JButton option = new JButton();
+			option.setText("Options");
+			option.setBackground(Color.WHITE);
+
+			background.add(option);
+
+			final JButton quit = new JButton();
+			quit.setText("Quit");
+			quit.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+					System.exit(0);
+				}
+			});
+
+			background.add(quit);
+
+			final JButton play = new JButton();
+			play.setText("Play");
+			play.setBackground(Color.WHITE);
+			background.add(play);
+			play.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+					// start game
+					speed = 2;
+					snakeColor = Color.RED;
+					backgroundColor = Color.BLACK;
+					board = new GameBoard(snakeColor, backgroundColor);
+
+					add(board);
+					setVisible(true);
+					background.setVisible(false);
+
+					ActionListener actListener = new ActionListener() {
+						public void actionPerformed(ActionEvent event) {
+							if (!isRunning()) {
+								timer.stop();
+							}
+							run();
+						}
+					};
+					setVisible(true);
+
+					Timer timer = new Timer(1000 / (speed * 10), actListener);
+					timer.start();
+
+					if (!timer.isRunning()) {
+						remove(board);
+						background.setVisible(true);
+
+					}
+
+					setVisible(true);
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		setVisible(true);
 	}
 
 }
